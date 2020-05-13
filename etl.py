a1b547ff7679bfb718161f8ae7098b6af93be70e
Tarek ALABRAS ALASHKAR <tarek.alashkar@gmail.com>
2020-05-14 01:31:57 +0200

    refactor: Add application name to spark session

--- etl.py
+++ etl.py
@@ -18,6 +18,7 @@ os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
 def create_spark_session():
     spark = SparkSession \
         .builder \
+        .appName('etl') \
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
         .getOrCreate()
     return spark
